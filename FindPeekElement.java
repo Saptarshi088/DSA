f3aea14c48ef08a443b37aa7d2b19e539336ff17
Saptarshi <saptarshibiswas6@gmail.com>
2025-09-15 12:40:39 +0530

    FindPeekElement.java

--- FindPeekElement.java
+++ FindPeekElement.java
@@ -1,10 +1,12 @@
 public class FindPeekElement {
-    
+
     public static void main(String[] args) {
         int[] nums = { 1, 2, 1, 3, 5, 6, 4 };
         System.out.println(findPeekElement(nums));
     }
 
+    // time complexity: O(logn)
+    // space complexity: O(1)
     public static int findPeekElement(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
