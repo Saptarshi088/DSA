00852c89e90fdd4fcf3dc31906f5b18966af8461
Saptarshi <saptarshibiswas6@gmail.com>
2025-09-15 12:39:59 +0530

    Find Peek Element

---
+++ FindPeekElement.java
@@ -0,0 +1,21 @@
+public class FindPeekElement {
+    
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 1, 3, 5, 6, 4 };
+        System.out.println(findPeekElement(nums));
+    }
+
+    public static int findPeekElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
