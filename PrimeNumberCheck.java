d7b4ca56d5420b83488952ee325f06ebf252be27
Saptarshi <saptarshibiswas6@gmail.com>
2025-09-16 20:03:24 +0530

    Check if a number is Prime ot Not

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,15 @@
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        int n = 17;
+        System.out.println(isPrime(n));
+    }
+
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
