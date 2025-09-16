08c0f29e4eb8dfd3a5ba32a861b69cb578ab9f2d
Saptarshi <saptarshibiswas6@gmail.com>
2025-09-16 20:43:28 +0530

    CoinChange.java: Added comments for time and space complexity

--- CoinChange.java
+++ CoinChange.java
@@ -1,22 +1,24 @@
+import java.util.Arrays;
+
 public class CoinChange {
+
     public static void main(String[] args) {
-        int coins[] = { 1, 2, 5 };
-        int ammpunt = 11;
-        System.out.println(coinChange(coins, ammpunt));
+        int coins[] = { 2, 3, 5 };
+        int ammount = 11;
+        System.out.println(coinChange(coins, ammount));
     }
 
-    public static int coinChange(int[] coins, int amount) {
-        int max = amount + 1;
-        int[] dp = new int[amount + 1];
-        for (int i = 1; i <= amount; i++) {
-            dp[i] = max;
-        }
+    // Time Complexity: O(n*amount)
+    // Space Complexity: O(amount)
+    public static int coinChange(int[] coins, int ammount) {
+        int dp[] = new int[ammount + 1];
+        Arrays.fill(dp, ammount + 1);
         dp[0] = 0;
         for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
+            for (int i = coin; i <= ammount; i++) {
                 dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[ammount] > ammount ? -1 : dp[ammount];
     }
 }
